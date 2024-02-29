ff758b61727ae3349a4435782e5045ace13554c4
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-02-29 12:53:05 +0000

    wip

--- src/utils/md6.ts
+++ src/utils/md6.ts
@@ -1,5 +1,7 @@
 import { createHash } from "crypto";
 
 export function MD6(input: string) {
-    return createHash("md5").update(btoa(encodeURIComponent(input))).digest("hex");
+	return createHash("md5")
+		.update(btoa(encodeURIComponent(input)))
+		.digest("hex");
 }
