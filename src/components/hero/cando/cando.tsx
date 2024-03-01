5142bd48209bbc7d169ff97161d278f4fdfe3527
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-01 07:51:49 +0000

    add

---
+++ src/components/hero/cando/cando.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useRef } from "react";
+import { candoList } from "./candoData";
+import { cn } from "@/src/utils/utils";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { Card, CardContent, CardHeader, CardTitle } from "@/src/components/ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/src/components/ui/carousel";
+
+export function Cando() {
+	return (
+		<>
+			<div className="w-full min-h-[500px] flex justify-center items-center">
+				<ParallaxBackGround />
+				<Card className="w-4/5 sm:w-3/4 h-4/5 sm:h-3/4 min-h-[400px] bg-transparent backdrop-blur-sm">
+					<CardHeader>
+						<CardTitle>What I can do 🤔</CardTitle>
+					</CardHeader>
+					<CardContent className="w-full flex justify-center items-center">
+						<Carousel className="w-full max-w-xl md:max-w-3xl">
+							<CarouselContent>
+								{candoList.map((cando, index) => (
+									<CarouselItem key={index} className="basis-full md:basis-1/2">
+										<div className="p-1">
+											<Card>
+												<CardContent className="flex flex-col justify-center aspect-square">
+													<Label className="text-xl font-bold mt-4">{cando.title}</Label>
+													<hr className="mt-1" />
+													<div className="mt-3 text-xs sm:text-sm leading-5">
+														{cando.description}
+													</div>
+													<div className="mt-auto pt-[-10px]">{cando.button}</div>
+												</CardContent>
+											</Card>
+										</div>
+									</CarouselItem>
+								))}
+							</CarouselContent>
+							<CarouselPrevious />
+							<CarouselNext />
+						</Carousel>
+					</CardContent>
+				</Card>
+			</div>
+		</>
+	);
+}
+
+function ParallaxBackGround() {
+	const triggerRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		const trigger = triggerRef.current;
+		if (!trigger) {
+			return;
+		}
+
+		gsap.registerPlugin(ScrollTrigger);
+
+		gsap.fromTo(
+			trigger,
+			{
+				yPercent: 50
+			},
+			{
+				yPercent: -50,
+				scrollTrigger: {
+					trigger: trigger,
+					start: "top bottom",
+					end: "bottom top",
+					scrub: 1
+				}
+			}
+		);
+	}, []);
+
+	return (
+		<div className="absolute top-[600px] w-full h-screen z-[-1]" ref={triggerRef}>
+			<IconObject name="nextjs/nextjs-original" className="relative top-[120px] left-[2.5vw]" />
+			<IconObject
+				name="github/github-original"
+				className="relative top-[100px] left-[97.5%] translate-x-[-100%]"
+			/>
+			<IconObject name="react/react-original" className="relative top-[120px] left-[3vw]" />
+			<IconObject
+				name="typescript/typescript-original"
+				className="relative top-[100px] left-[97.5%] translate-x-[-100%] rounded-md"
+			/>
+			<IconObject
+				name="denojs/denojs-original"
+				className="relative top-[120px] left-[2.5vw] bg-white rounded-full"
+			/>
+			<IconObject name="nodejs/nodejs-plain" className="relative top-[100px] left-[97.5%] translate-x-[-100%]" />
+			<IconObject name="vscode/vscode-original" className="relative top-[120px] left-[2.5vw]" />
+			<IconObject name="bash/bash-plain" className="relative top-[100px] left-[97.5%] translate-x-[-100%]" />
+		</div>
+	);
+}
+
+function IconObject({ name, className, ref }: { name: string; className?: string; ref?: React.Ref<HTMLImageElement> }) {
+	return (
+		<img
+			ref={ref}
+			src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/" + name + ".svg"}
+			className={cn("transition-all transiton-duration-300 drop-shadow-xl", className)}
+			style={{
+				filter: "drop-shadow(0 0 0.1rem white)"
+			}}
+			width={75}
+			height={75}
+			alt={name}
+		/>
+	);
+}
---
+++ src/components/hero/cando/candoData.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@/src/components/ui/button";
+
+export const candoList = [
+	{
+		title: "Create Website",
+		description: (
+			<div>
+				<p>
+					I can create websites in <b className="mx-1">Deno</b> or <b className="mx-1">Node.js</b>.
+				</p>
+				<p>
+					This site was created by combining <b className="mx-1">Astro.js</b> with various frameworks.
+				</p>
+			</div>
+		),
+		button: (
+			<a href={`https://github.com/EdamAme-x`} target="_blank">
+				<Button>All projects</Button>
+			</a>
+		)
+	},
+	{
+		title: "Create Server",
+		description: (
+			<div>
+				<p>
+					I can create server in <b className="mx-1">Deno</b> or <b className="mx-1">Node.js</b> and etc.
+				</p>
+				<p>
+					This site was created by combining <b className="mx-1">Astro.js</b> with{" "}
+					<b className="mx-1 text-orange-400">Hono</b>.
+				</p>
+			</div>
+		),
+		button: (
+			<a
+				href={`https://github.com/EdamAme-x?tab=repositories&q=&type=&language=typescript&sort=`}
+				target="_blank">
+				<Button>All projects</Button>
+			</a>
+		)
+	},
+	{
+		title: "Create Desktop App",
+		description: (
+			<div>
+				<p>
+					I can create server in <b className="mx-1">Deno</b> or <b className="mx-1">Node.js</b> and etc.
+				</p>
+				<p>
+					I use <b className="mx-1 text-red-500">Tauri</b> and <b className="mx-1 text-blue-300">Electron</b>.
+				</p>
+			</div>
+		),
+		button: (
+			<a
+				href={`https://github.com/EdamAme-x?tab=repositories&q=&type=&language=typescript&sort=`}
+				target="_blank">
+				<Button>All projects</Button>
+			</a>
+		)
+	},
+	{
+		title: "Create Tools for anything",
+		description: (
+			<div>
+				<p>
+					I have created tools for <b className="mx-1 text-green-500">LINE</b> and{" "}
+					<b className="mx-1 text-blue-600">Discord</b>.
+				</p>
+				<p>
+					Users reached <b className="mx-1">5,000</b> per day and SEO rose to first place.
+				</p>
+			</div>
+		),
+		button: (
+			<a href={`https://github.com/EdamAMe-x`} target="_blank">
+				<Button>All tools</Button>
+			</a>
+		)
+	}
+];
