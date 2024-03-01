485b70b71ae5853602c4a14007284db81116e07c
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-01 07:12:00 +0000

    add

--- src/components/hero/whoami/recordCard.tsx
+++ src/components/hero/whoami/recordCard.tsx
@@ -1,11 +1,11 @@
 import { Alert, AlertDescription, AlertTitle } from "@/src/components/ui/alert";
-import { CubeIcon } from "@radix-ui/react-icons";
+import { SunIcon } from "@radix-ui/react-icons";
 
-export function WhoamiCard() {
+export function RecordCard() {
 	return (
 			<Alert>
-				<CubeIcon className="h-4 w-4" />
-				<AlertTitle className="font-bold mb-2">Who am I ?</AlertTitle>
+				<SunIcon className="h-4 w-4" />
+				<AlertTitle className="font-bold mb-2">What do I have achievement ?</AlertTitle>
 				<AlertDescription>I'm a Web Developer, FullStack Developer & OSS Developer.</AlertDescription>
 				<AlertDescription>I have experience in building web applications & useful tool.</AlertDescription>
 			</Alert>
