270a8163d7d5952c318ea272089a03b3b396e37e
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-01 11:28:47 +0000

    add

--- src/components/hero/tech/techCard.tsx
+++ src/components/hero/tech/techCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/src/components/ui/card";
-import { Progress } from "@/src/components/ui/progress"
+import { Progress } from "@/src/components/ui/progress";
+import { Button } from "@/src/components/ui/button";
 
 export function TechCard() {
 	return (
@@ -8,36 +9,51 @@ export function TechCard() {
 				<CardTitle className="inline-flex gap-x-2">Technologies</CardTitle>
 			</CardHeader>
 			<CardContent className="w-full flex flex-col justify-center items-center gap-y-3">
-				<div className="flex gap-x-2">
+				<div className="flex gap-x-4 w-full">
 					<img
 						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/denojs/denojs-original.svg"
 						width="60"
 						alt="deno icon"
 						className="bg-white rounded-full"
 					/>
-                    <Progress />
+					<div className="w-full flex flex-col justify-around items-start">
+						<p className="text-lg">Deno</p>
+						<Progress value={90} className="w-full" />
+					</div>
 				</div>
-				<div className="flex gap-x-2">
+				<div className="flex gap-x-4 w-full">
 					<img
 						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
 						width="60"
 						alt="typescript icon"
 						className="rounded-md"
 					/>
-                    <Progress />
+					<div className="w-full flex flex-col justify-around items-start">
+						<p className="text-lg">TypeScript</p>
+						<Progress value={80} className="w-full" />
+					</div>
 				</div>
-				<div className="flex gap-x-2">
+				<div className="flex gap-x-4 w-full">
 					<img
 						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
 						width="60"
 						alt="react icon"
 					/>
-                    <Progress />
+					<div className="w-full flex flex-col justify-around items-start">
+						<p className="text-lg">React</p>
+						<Progress value={85} className="w-full" />
+					</div>
 				</div>
-				<div className="flex gap-x-2">
+				<div className="flex gap-x-4 w-full">
 					<img src="https://github.com/honojs.png" width="60" alt="hono icon" />
-                    <Progress />
+					<div className="w-full flex flex-col justify-around items-start">
+						<p className="text-lg">Hono</p>
+						<Progress value={95} className="w-full" />
+					</div>
 				</div>
+				<a href="/profile">
+					<Button variant="outline">See more</Button>
+				</a>
 			</CardContent>
 		</Card>
 	);
