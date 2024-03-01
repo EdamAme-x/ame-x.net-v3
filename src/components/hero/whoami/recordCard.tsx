de83541d40bbf85fb7b00fee7c766744cf3f5a7c
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-01 07:32:17 +0000

    add

--- src/components/hero/whoami/recordCard.tsx
+++ src/components/hero/whoami/recordCard.tsx
@@ -6,8 +6,8 @@ export function RecordCard() {
 			<Alert>
 				<SunIcon className="h-4 w-4" />
 				<AlertTitle className="font-bold mb-2">What do I have achievement ?</AlertTitle>
-				<AlertDescription className="text-xs md:text-sm my-1">I'm a Web Developer, FullStack Developer & OSS Developer.</AlertDescription>
-				<AlertDescription className="text-xs md:text-sm my-1">I have experience in building web applications & useful tool.</AlertDescription>
+				<AlertDescription className="text-xs md:text-sm my-1">I have achivement in operating a service that serves 5,000 people a day.</AlertDescription>
+				<AlertDescription className="text-xs md:text-sm my-1">The OSS I created has been used by people.</AlertDescription>
 			</Alert>
 	);
 }
