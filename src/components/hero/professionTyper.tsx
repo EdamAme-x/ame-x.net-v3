4dcea52442a0520666d760b5d35905c23a28ebef
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-01 04:54:51 +0000

    add type

--- src/components/hero/professionTyper.tsx
+++ src/components/hero/professionTyper.tsx
@@ -5,18 +5,15 @@ export function ProfessionTyper() {
 		<>
 			<TypeAnimation
 				sequence={[
-					"We produce food for Mice",
-					1000,
-					"We produce food for Hamsters",
-					1000,
-					"We produce food for Guinea Pigs",
-					1000,
-					"We produce food for Chinchillas",
-					1000,
+					"Web Developer",
+					1500,
+					"FullStack Developer",
+					1500,
+					"OSS Developer",
+					2500,
 				]}
 				wrapper="span"
 				speed={50}
-				style={{ fontSize: "2em", display: "inline-block" }}
 				repeat={Infinity}
 			/>
 		</>
