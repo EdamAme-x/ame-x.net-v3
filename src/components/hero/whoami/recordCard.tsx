e282dc614213d3c822815e79e053cfb7dbb6d43a
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-01 07:18:35 +0000

    add

--- src/components/hero/whoami/recordCard.tsx
+++ src/components/hero/whoami/recordCard.tsx
@@ -6,8 +6,8 @@ export function RecordCard() {
 			<Alert>
 				<SunIcon className="h-4 w-4" />
 				<AlertTitle className="font-bold mb-2">What do I have achievement ?</AlertTitle>
-				<AlertDescription>I'm a Web Developer, FullStack Developer & OSS Developer.</AlertDescription>
-				<AlertDescription>I have experience in building web applications & useful tool.</AlertDescription>
+				<AlertDescription className="text-xs md:text-sm my-1">I'm a Web Developer, FullStack Developer & OSS Developer.</AlertDescription>
+				<AlertDescription className="text-xs md:text-sm my-1">I have experience in building web applications & useful tool.</AlertDescription>
 			</Alert>
 	);
 }
--- src/components/hero/whoami/whoamiCard.tsx
+++ src/components/hero/whoami/whoamiCard.tsx
@@ -6,8 +6,8 @@ export function WhoamiCard() {
 			<Alert>
 				<CubeIcon className="h-4 w-4" />
 				<AlertTitle className="font-bold mb-2">Who am I ?</AlertTitle>
-				<AlertDescription>I'm a Web Developer, FullStack Developer & OSS Developer.</AlertDescription>
-				<AlertDescription>I have experience in building web applications & useful tool.</AlertDescription>
+				<AlertDescription className="text-xs md:text-sm my-1">I'm a Web Developer, FullStack Developer & OSS Developer.</AlertDescription>
+				<AlertDescription className="text-xs md:text-sm my-1">I have experience in building web applications & useful tool.</AlertDescription>
 			</Alert>
 	);
 }
